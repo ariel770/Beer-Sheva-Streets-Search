44679f9b72c1e6ecc06d56ef397b528a57c9a5b0
arielrosemerg <Pinjas31@gmail.com>
2026-02-05 11:43:59 +0200

    feat(backend): implement elasticsearch configuration and data mappings

---
+++ backend/src/models/StreetRecord.ts
@@ -0,0 +1,9 @@
+export interface StreetRecord {
+    id: string;
+    street_name: string;
+    neighborhood: string;
+    city: string;
+    type: string;
+    zip_code: string;
+    is_deleted: boolean;
+}
